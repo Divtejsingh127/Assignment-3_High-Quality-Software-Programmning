2e7317361a44296682ad2f15ec2462f20a5e4420
Divtej singh <divtejsingh126@gmail.com>
2024-03-17 23:48:02 -0400

    code change in truck.cs

--- Assignment 3_High Quality Software Programmning/Truck.cs
+++ Assignment 3_High Quality Software Programmning/Truck.cs
@@ -26,7 +26,7 @@ namespace Assignment_3_High_Quality_Software_Programmning
         public override void DisplayDetails()
         {
             base.DisplayDetails();
-            Console.WriteLine($"Capacity: {Capacity}, Truck Type: {TruckType}, Four Wheel Drive: {FourWheelDrive}");
+            Console.WriteLine($"Capacity: {Capacity}, Truck Types: {TruckType},The Four Wheel Drive: {FourWheelDrive}");
         }
     }
 }
